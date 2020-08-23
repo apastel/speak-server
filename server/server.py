f2ca56168390f03ea05c2ed60cb750373140f66d
apastel <alex.r.pastel@gmail.com>
2020-08-23 14:43:06 -0700

    Formatted logging

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import threading
 import datetime
+import logging
 from pygame import mixer
 from sense_hat import SenseHat
 from gtts import gTTS
@@ -12,15 +13,21 @@ app = Flask(__name__, static_folder='build')
 sense = SenseHat()
 quiet_hours_start = datetime.time(23, 30)
 quiet_hours_end = datetime.time(9, 0)
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M',
+    handlers=[logging.FileHandler('speak-server.log')]
+)
 
 @app.route('/api/submit', methods=['POST'])
 def submit():
-    print(f'Request received', flush=True)
+    logging.info('Request received')
     user_text = request.data.decode('utf-8')
     now = datetime.datetime.now()
     now_time = datetime.time(now.hour, now.minute, now.second)
     if time_in_range(quiet_hours_start, quiet_hours_end, now_time):
-        print(f'Received message during quiet hours: {user_text}', flush=True)
+        logging.info('Received message during quiet hours: %s', user_text)
     else:
         speak_thread = threading.Thread(target=speak, args=[user_text])
         display_thread = threading.Thread(target=show_message, args=[user_text])
@@ -29,7 +36,7 @@ def submit():
     return jsonify({"success": True})
 
 def speak(text_to_speak):
-    print(f'Speaking: {text_to_speak}', flush=True)
+    logging.info('Speaking: %s', text_to_speak)
     speech = gTTS(text_to_speak)
     fp = BytesIO()
     speech.write_to_fp(fp)
@@ -42,7 +49,7 @@ def speak(text_to_speak):
     fp.close()
 
 def show_message(text_to_display):
-    print(f'Displaying: {text_to_display}', flush=True)
+    logging.info('Displaying: %s', text_to_display)
     sense.set_rotation(180)
     sense.show_message(text_to_display, 0.05, text_colour=[0,0,255])
 
